0b00e8b83640cbf831fa923cbd77492f03af0c0a
Carl Ådahl <carl.adahl@flatfrog.com>
2024-08-30 12:42:47 +0200

    PrivateAddressCoordinator: Use 192.168.16.x subnet for WiFi P2P DHCP, and use .1 for ourselves.

--- packages/Tethering/src/com/android/networkstack/tethering/PrivateAddressCoordinator.java
+++ packages/Tethering/src/com/android/networkstack/tethering/PrivateAddressCoordinator.java
@@ -49,6 +49,7 @@ import java.util.Random;
  * @hide
  */
 public class PrivateAddressCoordinator {
+    private static final String TAG = PrivateAddressCoordinator.class.getSimpleName();
     public static final int PREFIX_LENGTH = 24;
 
     private static final int MAX_UBYTE = 256;
@@ -135,6 +136,39 @@ public class PrivateAddressCoordinator {
         }
 
         mUpstreamPrefixMap.removeAll(toBeRemoved);
+    }
+
+    @Nullable
+    public LinkAddress requestDownstreamAddressForWiFiP2p(final IpServer ipServer) {
+        maybeRemoveDeprectedUpstreams();
+
+        // Address would be 192.168.16.x/24.
+        final byte[] bytes = mTetheringPrefix.getRawAddress();
+        bytes[3] = (byte) 1;
+
+        final InetAddress addr;
+        try {
+            addr = InetAddress.getByAddress(bytes);
+        } catch (UnknownHostException e) {
+            throw new IllegalStateException("Invalid address, shouldn't happen.", e);
+        }
+
+        final IpPrefix prefix = new IpPrefix(addr, PREFIX_LENGTH);
+
+        // Check whether this prefix is in use.
+        if (isDownstreamPrefixInUse(prefix)) {
+            Log.e(TAG, "WiFi P2P prefix is already in use downstream.");
+            return null;
+        }
+
+        // Check whether this prefix is conflict with any current upstream network.
+        if (isConflictWithUpstream(prefix)) {
+            Log.e(TAG, "WiFi P2P prefix is in conflict with upstream network.");
+            return null;
+        }
+
+        mDownstreams.add(ipServer);
+        return new LinkAddress(addr, PREFIX_LENGTH);
     }
 
     /**
