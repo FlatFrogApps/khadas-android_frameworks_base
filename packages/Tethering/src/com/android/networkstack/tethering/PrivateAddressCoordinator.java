45dd0fe9cdcc31f8e84a6220483d8ebd9e0b0a10
Pablo Cases <pablo.cases@flatfrog.com>
2024-09-01 22:48:34 +0200

    PrivateAddressCoordinator.java: Add missing Log import statement

--- packages/Tethering/src/com/android/networkstack/tethering/PrivateAddressCoordinator.java
+++ packages/Tethering/src/com/android/networkstack/tethering/PrivateAddressCoordinator.java
@@ -37,6 +37,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import android.util.Log;
 
 /**
  * This class coordinate IP addresses conflict problem.
